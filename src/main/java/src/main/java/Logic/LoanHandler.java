8655caaa31b3a0160bc924e821ad3a46fad24ff7
DerDen12 <163834582+DerDen12@users.noreply.github.com>
2024-11-14 17:11:22 +0100

    Delete src/main/java/src/main/java directory

--- src/main/java/src/main/java/Logic/LoanHandler.java
+++
@@ -1,8 +0,0 @@
-package Logic;
-
-public interface LoanHandler {
-    void createLoan(Loan loan);
-    void reviewLoan(Loan loan);
-    void processLoan(Loan loan);
-    void denyLoan(Loan loan);
-}
